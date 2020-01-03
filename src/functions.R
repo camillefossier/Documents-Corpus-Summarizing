c55bc82ed73544e150b73fba5b62eb83221713ce
unknown <en_nebbache@esi.dz>
2020-01-03 17:51:38 +0100

    added search function by person, place and event

--- src/functions.R
+++ src/functions.R
@@ -63,6 +63,65 @@ search <- function(phrase, corpus, vocab, dtm, k=10) {
   ord
 }
 
+
+
+
+
+get_doc_matrices <- function(entity_dataset){
+  ent_dataset = entity_dataset[, -2]
+  person_matrix= get_doc_matrix(ent_dataset, "PERSON")
+  event_matrix = get_doc_matrix(ent_dataset, "EVENT")
+  place_matrix = get_doc_matrix(ent_dataset, "GPE")
+  
+  list(PERSON=person_matrix,EVENT=event_matrix, PLACE=place_matrix)
+  
+} 
+
+get_doc_matrix <- function(ent_dataset, type){
+  require(Matrix)
+  
+  num_docs = max(unique(ent_dataset$doc_id))
+  person_data = ent_dataset[ent_dataset$entity_type == type, ]
+  person_count = group(person_data)
+  entities = unique(person_count$entity)
+  mat = matrix(data = 0, nrow= length(entities), ncol= num_docs )
+  rownames(mat) = entities
+  for(i in 1:nrow(person_count)){
+    line = person_count[i,]
+    name = str_split(line[[2]], "\n")[[1]]
+    mat[name, line[[1]]] = as.numeric(line[[3]])
+  }
+  
+  res = Matrix(mat,sparse=T)
+  res
+  
+}
+
+group <- function(dataset) {
+  dataset %>%
+    group_by(doc_id, entity) %>%
+    summarise(num = length(entity))
+  
+}
+
+get_nearest <- function(index, category, number_of_suggestions, list_of_matrix ) {
+  mat = list_of_matrix[[category]]
+  vec = mat[index,]
+  if(sum(vec) == 0)
+    return (NULL)
+  else {
+    res = list()
+    for(cat in names(list_of_matrix)){
+      domain_matrix = list_of_matrix[[cat]]
+      suggestions = domain_matrix %*% vec
+      all_names  = rownames(domain_matrix)
+      names = all_names[order(suggestions, decreasing = T)][1:number_of_suggestions]
+      res[[cat]] = names
+    }
+    return(res)
+  }
+}
+
 ###############################################################
 
 to_trimestrial_dates <- function(dates) {
@@ -84,3 +143,5 @@ group_by_dates <- function(dataframe, ...) {
     summarise(...)
   amounts
 }
+
+
